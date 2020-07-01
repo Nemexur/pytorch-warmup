7a1c8c32bc3123b8e40c0281711044699d751876
nemexur <alex.milogradsky@gmail.com>
2020-07-01 21:21:37 +0300

    Added linear, cosine and constant + updated docs

--- schedulers/warmup_scheduler.py
+++ schedulers/warmup_scheduler.py
@@ -1,12 +1,17 @@
+from typing import Type
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler, LambdaLR
+from torch.optim.lr_scheduler import (
+    _LRScheduler, LambdaLR,
+    CosineAnnealingLR
+)
 from .combine_scheduler import CombineLRSchedulers
 
 
 class WarmUpScheduler(CombineLRSchedulers):
     """
     Gradual WarmUp Learning Rate Scheduler proposed in
-    `Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour` (https://arxiv.org/abs/1706.02677).
+    `Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour`
+    (https://arxiv.org/abs/1706.02677).
 
     Parameters
     ----------
@@ -23,7 +28,7 @@ class WarmUpScheduler(CombineLRSchedulers):
         Denominator for warmup like in `Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour`
         where learning rate was 0.1 * kn / 256. 256 is a denominator.
         With this for Gradual WarmUp stage at the end we could achieve
-        learning rate higher than it was in `optimizer`.
+        learning rate higher than it was in `optimizer` at the last step.
         If None use `warmup_steps` as a denominator.
     """
     def __init__(
@@ -58,4 +63,109 @@ class WarmUpScheduler(CombineLRSchedulers):
         super().__init__(
             lr_schedulers=lr_schedulers,
             lr_schedulers_steps=lr_schedulers_steps
+        )
+
+    @classmethod
+    def with_linear_stage(
+        cls: Type['WarmUpScheduler'],
+        optimizer: Optimizer,
+        warmup_steps: int,
+        num_training_steps: int,
+        warmup_denominator: int = None
+    ) -> Type['WarmUpScheduler']:
+        """
+        Instantiate WarmUpScheduler with linear decreasing learning rate after it.
+
+        Parameters
+        ----------
+        optimizer : `Optimizer`, required
+            Wrapped optimizer.
+        warmup_steps : `int`, required
+            Number of steps for Gradual WarmUp stage.
+        num_training_steps : `int`, required
+            Number of steps in training phase.
+        warmup_denominator : `int`, optional (default = `None`)
+            Denominator for warmup like in `Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour`
+            where learning rate was 0.1 * kn / 256. 256 is a denominator.
+            With this for Gradual WarmUp stage at the end we could achieve
+            learning rate higher than it was in `optimizer` at the last step.
+            If None use `warmup_steps` as a denominator.
+        """
+        return cls(
+            optimizer=optimizer,
+            warmup_steps=warmup_steps,
+            after_warmup_scheduler=LambdaLR(
+                optimizer,
+                lr_lambda=lambda step: max(
+                    0.0,
+                    (num_training_steps - step) / max(1.0, num_training_steps - warmup_steps)
+                )
+            ),
+            warmup_denominator=warmup_denominator
+        )
+
+    @classmethod
+    def with_cosine_stage(
+        cls: Type['WarmUpScheduler'],
+        optimizer: Optimizer,
+        warmup_steps: int,
+        num_training_steps: int,
+        warmup_denominator: int = None,
+    ) -> Type['WarmUpScheduler']:
+        """
+        Instantiate WarmUpScheduler with cosine annealing learning rate after it.
+
+        Parameters
+        ----------
+        optimizer : `Optimizer`, required
+            Wrapped optimizer.
+        warmup_steps : `int`, required
+            Number of steps for Gradual WarmUp stage.
+        num_training_steps : `int`, required
+            Number of steps in training phase.
+        warmup_denominator : `int`, optional (default = `None`)
+            Denominator for warmup like in `Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour`
+            where learning rate was 0.1 * kn / 256. 256 is a denominator.
+            With this for Gradual WarmUp stage at the end we could achieve
+            learning rate higher than it was in `optimizer` at the last step.
+            If None use `warmup_steps` as a denominator.
+        """
+        return cls(
+            optimizer=optimizer,
+            warmup_steps=warmup_steps,
+            after_warmup_scheduler=CosineAnnealingLR(
+                optimizer,
+                T_max=num_training_steps
+            ),
+            warmup_denominator=warmup_denominator
+        )
+
+    @classmethod
+    def with_constant_stage(
+        cls: Type['WarmUpScheduler'],
+        optimizer: Optimizer,
+        warmup_steps: int,
+        warmup_denominator: int = None
+    ) -> Type['WarmUpScheduler']:
+        """
+        Instantiate WarmUpScheduler with constant learning rate after it.
+
+        Parameters
+        ----------
+        optimizer : `Optimizer`, required
+            Wrapped optimizer.
+        warmup_steps : `int`, required
+            Number of steps for Gradual WarmUp stage.
+        warmup_denominator : `int`, optional (default = `None`)
+            Denominator for warmup like in `Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour`
+            where learning rate was `0.1 * kn / 256`. 256 is a denominator.
+            With this for Gradual WarmUp stage at the end we could achieve
+            learning rate higher than it was in `optimizer` at the last step.
+            If None use `warmup_steps` as a denominator.
+        """
+        return cls(
+            optimizer=optimizer,
+            warmup_steps=warmup_steps,
+            after_warmup_scheduler=LambdaLR(optimizer, lr_lambda=lambda step: 1.0),
+            warmup_denominator=warmup_denominator
         )
